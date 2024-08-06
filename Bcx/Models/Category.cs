abcb3fda8fab9df5134300fa0b0f37040cb0ff63
Samuel Livingston <roadrustersam@gmail.com>
2024-08-06 20:06:29 +0530

    Add project files.

---
+++ Bcx/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bcx.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category_Id { get; set; }
+        public required string Category_Name { get; set; }
+        public int Category_Display_Order { get; set; }
+    }
+}
---
+++ Bcx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bcx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
