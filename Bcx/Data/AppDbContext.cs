2c4f32dfb286f87d6ba33430a081f2101b117662
Samuel Livingston <roadrustersam@gmail.com>
2024-08-07 19:36:42 +0530

    DbSet

--- Bcx/Data/AppDbContext.cs
+++ Bcx/Data/AppDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bcx.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bcx.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        {
-                
+        {       
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
